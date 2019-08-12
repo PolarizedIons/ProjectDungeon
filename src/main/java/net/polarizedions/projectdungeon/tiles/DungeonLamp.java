de395d605a88014fc88ae0b525d7f3eec6c44191
polarizedions <polarizedions@gmail.com>
2019-08-12 20:53:45 +0200

    Make tiles drop themselves

--- src/main/java/net/polarizedions/projectdungeon/tiles/DungeonLamp.java
+++ src/main/java/net/polarizedions/projectdungeon/tiles/DungeonLamp.java
@@ -1,10 +1,18 @@
 package net.polarizedions.projectdungeon.tiles;
 
+import de.ellpeck.rockbottom.api.entity.Entity;
+import de.ellpeck.rockbottom.api.entity.player.AbstractEntityPlayer;
+import de.ellpeck.rockbottom.api.item.ItemInstance;
+import de.ellpeck.rockbottom.api.tile.Tile;
 import de.ellpeck.rockbottom.api.tile.TileBasic;
 import de.ellpeck.rockbottom.api.util.BoundBox;
 import de.ellpeck.rockbottom.api.world.IWorld;
 import de.ellpeck.rockbottom.api.world.layer.TileLayer;
 import net.polarizedions.projectdungeon.init.Resources;
+import net.polarizedions.projectdungeon.init.Tiles;
+
+import java.util.Collections;
+import java.util.List;
 
 public class DungeonLamp extends TileBasic {
     public DungeonLamp() {
@@ -24,5 +32,25 @@ public class DungeonLamp extends TileBasic {
     @Override
     public boolean isFullTile() {
         return false;
+    }
+
+    @Override
+    public boolean canPlaceInLayer(TileLayer layer) {
+        return layer == TileLayer.MAIN;
+    }
+
+    @Override
+    public boolean canPlace(IWorld world, int x, int y, TileLayer layer, AbstractEntityPlayer player) {
+        return ! world.getState(TileLayer.BACKGROUND, x, y).getTile().isAir();
+    }
+
+    @Override
+    public boolean canStay(IWorld world, int x, int y, TileLayer layer, int changedX, int changedY, TileLayer changedLayer) {
+        return ! world.getState(TileLayer.BACKGROUND, x, y).getTile().isAir();
+    }
+
+    @Override
+    public List<ItemInstance> getDrops(IWorld world, int x, int y, TileLayer layer, Entity destroyer) {
+        return Collections.singletonList(new ItemInstance(Tiles.DUNGEON_LAMP));
     }
 }
--- src/main/java/net/polarizedions/projectdungeon/tiles/DungeonWall.java
+++ src/main/java/net/polarizedions/projectdungeon/tiles/DungeonWall.java
@@ -2,31 +2,23 @@ package net.polarizedions.projectdungeon.tiles;
 
 import de.ellpeck.rockbottom.api.entity.Entity;
 import de.ellpeck.rockbottom.api.item.ItemInstance;
-import de.ellpeck.rockbottom.api.render.tile.ITileRenderer;
 import de.ellpeck.rockbottom.api.tile.TileBasic;
 import de.ellpeck.rockbottom.api.util.reg.ResourceName;
 import de.ellpeck.rockbottom.api.world.IWorld;
 import de.ellpeck.rockbottom.api.world.layer.TileLayer;
+import net.polarizedions.projectdungeon.init.Resources;
 import net.polarizedions.projectdungeon.init.Tiles;
-import net.polarizedions.projectdungeon.render.DungeonWallTrimRenderer;
 
 import java.util.Collections;
 import java.util.List;
 
-import static net.polarizedions.projectdungeon.init.Resources.DUNGEON_WALL_TRIM;
-
-public class DungeonWallTrim extends TileBasic {
-    public DungeonWallTrim() {
-        super(DUNGEON_WALL_TRIM);
-    }
-
-    @Override
-    protected ITileRenderer createRenderer(ResourceName name) {
-        return new DungeonWallTrimRenderer(name);
+public class DungeonWall extends TileBasic {
+    public DungeonWall() {
+        super(Resources.DUNGEON_WALL);
     }
 
     @Override
     public List<ItemInstance> getDrops(IWorld world, int x, int y, TileLayer layer, Entity destroyer) {
-        return Collections.singletonList(new ItemInstance(Tiles.DUNGEON_WALL_TRIM));
+        return Collections.singletonList(new ItemInstance(Tiles.DUNGEON_WALL));
     }
 }
